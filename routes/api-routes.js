c53b7705b9904c2b3c1e47945a446b5beb9b6770
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-20 05:01:05 -0500

    Add get routes: confirm functional

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,49 @@
 const router = require('express').Router();
-const apiRoutes = require('./api-routes');
+// const apiRoutes = require('./api-routes');
+// router.use('/api', apiRoutes);
+const Workout = require('../models/Workout');
 
-router.use('/api', apiRoutes);
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// router.put('/api/workouts', (req, res) => {
+//     Workout.findByIdAndUpdate(req.params.id,
+//         { $push: { exercises: req.body } },
+//         {new: true})
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+
+// router.post('/api/workouts')
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                durationSnapshot: { $sum: '$exercises.duration' },
+                weightSnapshot: { $sum: '$exercises.weight' }
+            }
+        }
+    ])
+        .limit(7)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 
 module.exports = router;
