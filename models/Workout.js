ebe9bc99251f44efae7ed71b7d6cdc015ee76875
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-20 04:29:09 -0500

    Docs: rename capitalized to follow model naming convention

--- models/Workout.js
+++ models/Workout.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+// *NOTE: reference seed file to understand how to write model definition
 
+
+// *NOTE: exerciseSchema is a sub-field of workoutSchema
+//        thus, able to condense to one model
+//        -- define sub-field first in order to reference in workoutSchema
 const exerciseSchema = new Schema({
 
-    type: { type: String, required: true }, //make first field object //bug correction
+    type: { type: String, required: true }, //make first field object //bug work-around
     name: { type: String, required: true },
     duration: { type: Number, required: true },
     distance: Number,
@@ -15,7 +20,7 @@ const exerciseSchema = new Schema({
 
 const workoutSchema = new Schema({
     day: { type: Date, default: Date.now },
-    exercises: { type: [exerciseSchema] }
+    exercises: { type: [exerciseSchema] } //array of exercises
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
