35f92888134c1499b4c499dc71cbf5946066f9e9
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-05-20 03:45:23 -0500

    Rename routes + update filepaths

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const logger = require('morgan');
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,12 +15,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/view-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
